41ea47fb96efb43ef61856a3f8e9263cb9388b73
Tina Hang <mstinahang@gmail.com>
2020-01-09 03:28:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+function updateDriverWithKeyAndValue(driver, key, value) {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
+}
+
+function deleteFromDriverByKey(driver, key) {
+  
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  
+}
