c394c6a02a5050aee49339e3ae0996390ead783f
Tina Hang <mstinahang@gmail.com>
2020-01-09 03:32:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-
+  driver[key] = value;
+  return driver;
 }
 
 function deleteFromDriverByKey(driver, key) {
@@ -14,5 +15,6 @@ function deleteFromDriverByKey(driver, key) {
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-
+  delete driver.key;
+  return driver;
 }
